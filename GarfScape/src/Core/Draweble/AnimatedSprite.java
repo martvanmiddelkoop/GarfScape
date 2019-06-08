39270ca02005a2bee5a2964054f3b8c767b179d7
GeestWagen <nkolpa12@gmail.com>
2019-06-09 00:53:19 +0200

    animated sprites toegevoegd (ik weet niet of ze werken)

--- GarfScape/src/Core/Draweble/AnimatedSprite.java
+++ GarfScape/src/Core/Draweble/AnimatedSprite.java
@@ -1,9 +1,38 @@
 package Core.Draweble;
 
+import Core.Data.Shader;
+import Core.Data.Texture;
+import java.util.ArrayList;
 
-/*
 public class AnimatedSprite extends Sprite2D
 {//TODO IMPLEMENT THIS
 
+    int index = 0;
+    ArrayList<Texture> textureArray;
+
+    public AnimatedSprite(Shader shader, ArrayList<Texture> textureArray)
+    {
+        super(textureArray.get(0), shader);
+        this.textureArray = textureArray;
+    }
+
+    @Override
+    public void setTexture(Texture texture)
+    {
+        this.texture = textureArray.get(textureArray.indexOf(texture));
+    }
+
+    public void step()
+    {
+        if (index >= textureArray.size() - 1)
+        {
+            this.texture = textureArray.get(index++);
+        }
+        else
+        {
+            index = 0;
+            this.texture = textureArray.get(index);
+        }
+    }
+
 }
-*/
